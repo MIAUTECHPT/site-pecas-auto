faddf3694e268b8173e8f9e3e214d64d1a9d7899
MiautechPT <miautechpt@hotmail.com>
2026-08-26 00:06:50 +0100

    Ajustar configuracao do Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({ adapter })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
-export default prisma;
+export default prisma
